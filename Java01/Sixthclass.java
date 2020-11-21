d146a93fae2e7e1b6eb3c766e4d68c1df267e121
VARISH GAUTAM <48176176+Variiiest@users.noreply.github.com>
2020-11-22 01:20:47 +0530

    Create Sixthclass.java

---
+++ Java01/Sixthclass.java
@@ -0,0 +1,8 @@
+//String 
+public class Main {
+  public static void main(String[] args) {
+    String firstName = "John ";
+    String lastName = "Doe";
+    System.out.println(firstName.concat(lastName));
+  }
+}
