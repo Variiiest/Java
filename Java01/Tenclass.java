185286ef443640c947ea29f7b00db060d4748762
VARISH GAUTAM <48176176+Variiiest@users.noreply.github.com>
2020-11-22 01:31:16 +0530

    Create Tenclass.java

---
+++ Java01/Tenclass.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args) {
+    String[] charts = {"Bar", "Pie", "Hist", "Scatter"};
+    for (String i : charts) {
+      System.out.println(i);
+    }    
+  }
+}
