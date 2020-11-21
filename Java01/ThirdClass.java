2455e775cb0ccb9a947cca5a8418b36951c2d471
VARISH GAUTAM <48176176+Variiiest@users.noreply.github.com>
2020-11-22 01:14:27 +0530

    Create ThirdClass.java

---
+++ Java01/ThirdClass.java
@@ -0,0 +1,11 @@
+// Data types are divided into two groups:
+
+// Primitive data types - includes byte, short, int, long, float, double, boolean and char
+// Non-primitive data types - such as String, Arrays and Classes
+
+public class Main {
+  public static void main(String[] args) {
+    byte myNum = 1547;//through a error -127 to 128
+    System.out.println(myNum);  
+  }
+}
