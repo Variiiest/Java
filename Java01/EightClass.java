f06a1dc6dfef9a938b9d6804c40ed274bd0e9bcb
VARISH GAUTAM <48176176+Variiiest@users.noreply.github.com>
2020-11-22 01:24:00 +0530

    Create EightClass.java

---
+++ Java01/EightClass.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args) {
+    boolean JavaisFun = true;
+    boolean PythonisFun = false;    
+    System.out.println(JavaisFun);
+    System.out.println(PythonisFun);
+  }
+}
